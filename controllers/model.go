858de2363d4e969565d71884bf71545392fe0534
grald003 <josephgeraldo2003@gmail.com>
2023-03-23 17:53:55 +0700

    Linter

--- controllers/model.go
+++ controllers/model.go
@@ -1,5 +1,6 @@
 package controllers
 
+//User is the model for user
 type User struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
@@ -8,24 +9,29 @@ type User struct {
 	Country string `json:"country"`
 }
 
+//UsersResponse is the response for get all users
 type UsersResponse struct {
 	Status  int `json:"status"`
 	Message string `json:"message"`
 	Data    []User `json:"data"`
 }
 
+//ErrorResponse is the response for error
 type ErrorResponse struct {
 	Status  int `json:"status"`
 	Message string `json:"message"`
 }
+
+//Response is the response for get, post, put
 type Response struct {
 	Status  int `json:"status"`
 	Message string `json:"message"`
 	Data    User `json:"data"`
 }
 
+//ResponseDelete is the response for delete
 type ResponseDelete struct {
 	Status  int `json:"status"`
 	Message string `json:"message"`
-	Id	    int `json:"id"`
+	ID	    int `json:"id"`
 }
