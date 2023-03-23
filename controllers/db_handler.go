9e9917e348b8f27df55a55d1cd20cfece3b9d294
grald003 <josephgeraldo2003@gmail.com>
2023-03-23 14:06:31 +0700

    TugasEcho

--- controllers/db_handler.go
+++ controllers/db_handler.go
@@ -5,8 +5,9 @@ import (
 	"log"
 )
 
-//Untuk Connect ke Database
+//Untuk Connect() ke Database
 func Connect() *sql.DB {
+	//Membuka koneksi ke database dengan sql.Open()
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/tugasecho")
 	if err != nil {
 		log.Fatal(err)
